7b8798580c7feff7a007e9b7f5b0580625ca9e4b
Luan Costa <luancosta15000@gmail.com>
2022-11-10 20:02:41 -0300

    commit inicial

---
+++ jsfile.js
@@ -0,0 +1,88 @@
+const myslide = document.querySelectorAll('.myslide'),
+	  dot = document.querySelectorAll('.dot');
+let counter = 1;
+slidefun(counter);
+
+let timer = setInterval(autoSlide, 8000);
+function autoSlide() {
+	counter += 1;
+	slidefun(counter);
+}
+function plusSlides(n) {
+	counter += n;
+	slidefun(counter);
+	resetTimer();
+}
+function currentSlide(n) {
+	counter = n;
+	slidefun(counter);
+	resetTimer();
+}
+function resetTimer() {
+	clearInterval(timer);
+	timer = setInterval(autoSlide, 8000);
+}
+
+function slidefun(n) {
+	
+	let i;
+	for(i = 0;i<myslide.length;i++){
+		myslide[i].style.display = "none";
+	}
+	for(i = 0;i<dot.length;i++) {
+		dot[i].className = dot[i].className.replace(' active', '');
+	}
+	if(n > myslide.length){
+	   counter = 1;
+	   }
+	if(n < 1){
+	   counter = myslide.length;
+	   }
+	myslide[counter - 1].style.display = "block";
+	dot[counter - 1].className += " active";
+}
+class MobileNavbar {
+	constructor(mobileMenu, navList, navLinks) {
+	  this.mobileMenu = document.querySelector(mobileMenu);
+	  this.navList = document.querySelector(navList);
+	  this.navLinks = document.querySelectorAll(navLinks);
+	  this.activeClass = "active";
+  
+	  this.handleClick = this.handleClick.bind(this);
+	}
+  
+	animateLinks() {
+	  this.navLinks.forEach((link, index) => {
+		link.style.animation
+		  ? (link.style.animation = "")
+		  : (link.style.animation = `navLinkFade 0.5s ease forwards ${
+			  index / 7 + 0.3
+			}s`);
+	  });
+	}
+  
+	handleClick() {
+	  this.navList.classList.toggle(this.activeClass);
+	  this.mobileMenu.classList.toggle(this.activeClass);
+	  this.animateLinks();
+	}
+  
+	addClickEvent() {
+	  this.mobileMenu.addEventListener("click", this.handleClick);
+	}
+  
+	init() {
+	  if (this.mobileMenu) {
+		this.addClickEvent();
+	  }
+	  return this;
+	}
+  }
+  
+  const mobileNavbar = new MobileNavbar(
+	".mobile-menu",
+	".nav-list",
+	".nav-list li",
+  );
+  mobileNavbar.init();
+  
