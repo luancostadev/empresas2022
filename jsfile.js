a03a58d1ee549242fc97c26e8065d836c220b354
EnzoTrulenque <enzotrulenque@gmail.com>
2022-11-11 16:21:59 -0300

    commit celular 2

--- jsfile.js
+++ jsfile.js
@@ -85,4 +85,18 @@ class MobileNavbar {
 	".nav-list li",
   );
   mobileNavbar.init();
+
+  function setColor(btn, color){
+    var count=1;
+    var property = document.getElementById(btn);
+    if (count == 0){
+        property.style.backgroundColor = "#FFFFFF"
+        count=1;        
+    }
+    else{
+        property.style.backgroundColor = "#7FFF00"
+        count=0;
+    }
+
+}
   
